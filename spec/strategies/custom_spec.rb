82115cf8ff8df32353471de2f7fd38a6fcea5139
Jackson Harper <jackson@juvo.com>
2016-08-20 16:09:33 -0700

    Clean up tests

--- spec/strategies/custom_spec.rb
+++ spec/strategies/custom_spec.rb
@@ -12,19 +12,15 @@ class CustomStrategy
 end
 
 describe CB2::Breaker do
-  let(:breaker) do
-
-  end
-
   describe "#initialize" do
-    it "initializes the strategy" do
+    it "initializes a custom strategy" do
       breaker = CB2::Breaker.new(
           strategy:  CustomStrategy,
           duration:  60,
           threshold: 10,
           reenable_after: 600)
-      assert breaker.strategy.is_a?(CustomStrategy)
       assert breaker.strategy.initialized
+      assert breaker.strategy.is_a?(CustomStrategy)
     end
   end
 end
